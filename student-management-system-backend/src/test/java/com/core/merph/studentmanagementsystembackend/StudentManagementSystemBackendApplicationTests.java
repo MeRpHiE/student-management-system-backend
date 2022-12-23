7119282fc79563459c3c72f4b9b9c1105d4603e2
MeRpHiE <asiri.abayakoon@gmail.com>
2022-12-23 23:20:03 +0530

    Adding missing files for initial skeleton.

---
+++ student-management-system-backend/src/test/java/com/core/merph/studentmanagementsystembackend/StudentManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.merph.studentmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
